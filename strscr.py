27e00367746a465cdd61edd2bc6d7a0dbfd8a9e2
anopsy <anopsy28@gmail.com>
2023-04-15 16:16:07 +0200

    script scrapes HAR-file multiple times, defined by i

--- strscr.py
+++ strscr.py
@@ -21,39 +21,46 @@ caps = DesiredCapabilities.CHROME.copy()
 caps['acceptSslCerts'] = True
 caps['acceptInsecureCerts'] = True
 
-proxy.new_har(ref=None,options={'captureHeaders': True,'captureContent':True,'captureBinaryContent': True}) # tag network logs 
+numofhar=0
+i=58
+while numofhar<40:
+
+    proxy.new_har(ref=None,options={'captureHeaders': True,'captureContent':True,'captureBinaryContent': True}) # tag network logs 
               
-driver = webdriver.Chrome('chromedriver',options=options,desired_capabilities=caps)
-driver.get(url)
+    driver = webdriver.Chrome('chromedriver',options=options,desired_capabilities=caps)
+    driver.get(url)
+    numofhar+=1
 
-fetched = []
-i=0
-for ent in proxy.har['log']['entries']:
-    i=i+1
-    _url = ent['request']['url']
-    _response = ent['response']
+    fetched = []
     
-    #make sure havent already downloaded this piece
-    if _url in fetched:
-        continue
-        
-    if _url.endswith('.ts'):
-        #check if this url had a valid response, if not, ignore it
-        if 'text' not in _response['content'].keys() or not _response['content']['text']:
+    for ent in proxy.har['log']['entries']:
+    
+        _url = ent['request']['url']
+        _response = ent['response']
+    
+        #make sure havent already downloaded this piece
+        if _url in fetched:
             continue
+        
+        if _url.endswith('.ts'):
+            #check if this url had a valid response, if not, ignore it
+            if 'text' not in _response['content'].keys() or not _response['content']['text']:
+                continue
             
-        print(_url+'\n')
-        r1 = requests.get(_url, stream=True)
-        if(r1.status_code == 200 or r1.status_code == 206):
+            print(_url+'\n')
+            r1 = requests.get(_url, stream=True)
+            if(r1.status_code == 200 or r1.status_code == 206):
 
-            # re-open output file to append new video
-            f = open("/home/anopsy/data/autodata/{}".format(i),"wb")
-            data = b''
-            for chunk in r1.iter_content(chunk_size=1024):
-                if(chunk):
-                    data += chunk
-            f.write(data)
-            f.close()
-            fetched.append(_url)
-        else:
-            print("Received unexpected status code {}".format(r1.status_code))  
+                # re-open output file to append new video
+                f = open("/home/anopsy/stream-scraper/strscr/{}wave.ts".format(i),"wb")
+                print("File created: " + "/home/anopsy/stream-scraper/strscr/{}wave.ts".format(i))
+                i+=1
+                data = b''
+                for chunk in r1.iter_content(chunk_size=1024):
+                    if(chunk):
+                        data += chunk
+                f.write(data)
+                f.close()
+                fetched.append(_url)
+            else:
+                print("Received unexpected status code {}".format(r1.status_code))  
